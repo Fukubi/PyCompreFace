72720d94f972c6d69c049ebc8470ded4152cc39b
Deivid Gabriel <deividgabrielproc@gmail.com>
2026-01-08 10:25:06 -0300

    feat: Image recognition support added

--- pycompreface/services/CompreFaceRecognition.py
+++ pycompreface/services/CompreFaceRecognition.py
@@ -1,7 +1,8 @@
 import requests
 
 from pycompreface.types.FaceRecognitionResponse import (
-    FaceRecognitionAddExampleResponse, FaceRecognitionListExamplesResponse)
+    FaceRecognitionAddExampleResponse, FaceRecognitionListExamplesResponse,
+    FaceRecognitionResponse)
 
 
 class CompreFaceRecognition:
@@ -10,20 +11,70 @@ class CompreFaceRecognition:
     Attributes:
         api_key (str): The API key gotten from the CompreFace application for the FaceDetection
         host (str): The complete hostname of the CompreFace application
+        limit (:obj:`int`, optional): The max number of faces to be detected (default is unlimited)
+        det_prob_threshold (:obj:`float`, optional): The threshold of the probability of it being a
+            face so it can be recognized as such (default is 0.7)
+        prediction_count (:obj:`int`, optional): The max number of subjects from a prediction
+        use_age_plugin (:obj:`bool`, optional): If the age plugin should be used (default is False)
+        use_gender_plugin (:obj:`bool`, optional): If the gender plugin should be used (default is False)
+        use_landmarks_plugin (:obj:`bool`, optional): If the landmarks plugin should be used (default is False)
+        use_calculator_plugin (:obj:`bool`, optional): If the calculator plugin should be used (default is False)
+        use_pose_plugin (:obj:`bool`, optional): If the pose plugin should be used (default is False)
+        use_mask_plugin (:obj:`bool`, optional): If the mask plugin should be used (default is False)
     """
 
     api_key: str
     host: str
+    limit: int
+    det_prob_threshold: float
+    prediction_count: int
+    use_age_plugin: bool
+    use_gender_plugin: bool
+    use_landmarks_plugin: bool
+    use_calculator_plugin: bool
+    use_pose_plugin: bool
+    use_mask_plugin: bool
 
-    def __init__(self, api_key: str, host: str) -> None:
+    def __init__(
+        self,
+        api_key: str,
+        host: str,
+        limit: int = 0,
+        det_prob_threshold: float = 0.7,
+        prediction_count: int = 1,
+        use_age_plugin: bool = False,
+        use_gender_plugin: bool = False,
+        use_landmarks_plugin: bool = False,
+        use_calculator_plugin: bool = False,
+        use_pose_plugin: bool = False,
+        use_mask_plugin: bool = False,
+    ) -> None:
         """Default Constructor
 
         Args:
             api_key (str): The API key gotten from the CompreFace application for the FaceDetection
             host (str): The complete hostname of the CompreFace application
+            det_prob_threshold (:obj:`float`, optional): The threshold of the probability of it being a
+                face so it can be recognized as such (default is 0.7)
+            prediction_count (:obj:`int`, optional): The max number of subjects from a prediction
+            use_age_plugin (:obj:`bool`, optional): If the age plugin should be used (default is False)
+            use_gender_plugin (:obj:`bool`, optional): If the gender plugin should be used (default is False)
+            use_landmarks_plugin (:obj:`bool`, optional): If the landmarks plugin should be used (default is False)
+            use_calculator_plugin (:obj:`bool`, optional): If the calculator plugin should be used (default is False)
+            use_pose_plugin (:obj:`bool`, optional): If the pose plugin should be used (default is False)
+            use_mask_plugin (:obj:`bool`, optional): If the mask plugin should be used (default is False)
         """
         self.api_key = api_key
         self.host = host
+        self.limit = limit
+        self.det_prob_threshold = det_prob_threshold
+        self.prediction_count = prediction_count
+        self.use_age_plugin = use_age_plugin
+        self.use_gender_plugin = use_gender_plugin
+        self.use_landmarks_plugin = use_landmarks_plugin
+        self.use_calculator_plugin = use_calculator_plugin
+        self.use_pose_plugin = use_pose_plugin
+        self.use_mask_plugin = use_mask_plugin
 
     def add_subject(self, subject_name: str) -> str:
         """Add a new subject to the CompreFace
@@ -205,3 +256,65 @@ class CompreFaceRecognition:
         )
 
         return response.json()
+
+    def get_face_plugins(self) -> str:
+        """Generate the string for the query of the face plugins used
+
+        Returns:
+            str: The query string with the list of the face plugins used
+                ex: &face_plugins=age,gender,
+                Empty string if no plugins are used
+        """
+
+        if (
+            self.use_age_plugin
+            or self.use_gender_plugin
+            or self.use_landmarks_plugin
+            or self.use_calculator_plugin
+            or self.use_pose_plugin
+            or self.use_mask_plugin
+        ):
+            face_plugins = "&face_plugins="
+            if self.use_age_plugin:
+                face_plugins += "age,"
+            if self.use_gender_plugin:
+                face_plugins += "gender,"
+            if self.use_landmarks_plugin:
+                face_plugins += "landmarks,"
+            if self.use_calculator_plugin:
+                face_plugins += "calculator,"
+            if self.use_pose_plugin:
+                face_plugins += "pose,"
+            if self.use_mask_plugin:
+                face_plugins += "mask,"
+
+            return face_plugins
+        else:
+            return ""
+
+    def recognize_faces_from_image(self, image: str | bytes) -> FaceRecognitionResponse:
+        """Recognize the subjects from the requested image
+
+        Args:
+            image (str | bytes): The image filepath or the bytes of the image
+
+        Returns:
+            FaceRecognitionResponse: The default response from the recognize API route
+        """
+        if type(image) == str:
+            with open(image, "rb") as f:
+                response = requests.post(
+                    f"{self.host}/api/v1/recognition/recognize?limit={self.limit}&prediction_count={self.prediction_count}&det_prob_threshold={self.det_prob_threshold}&face_plugins={self.get_face_plugins()}",
+                    headers={"x-api-key": self.api_key},
+                    files={"file": f},
+                )
+
+                return response.json()
+        else:
+            response = requests.post(
+                f"{self.host}/api/v1/recognition/recognize?limit={self.limit}&prediction_count={self.prediction_count}&det_prob_threshold={self.det_prob_threshold}&face_plugins={self.get_face_plugins()}",
+                headers={"x-api-key": self.api_key},
+                files={"file": ("image.jpg", image)},
+            )
+
+            return response.json()
--- pycompreface/types/FaceRecognitionResponse.py
+++ pycompreface/types/FaceRecognitionResponse.py
@@ -1,4 +1,10 @@
-from typing import TypedDict
+from typing import List, TypedDict
+
+from pycompreface.types.FaceDetectionResult import (FaceDetectionAgeResult,
+                                                    FaceDetectionBoxResult,
+                                                    FaceDetectionExecutionTime,
+                                                    FaceDetectionGenderResult,
+                                                    FaceDetectionMaskResult)
 
 
 class FaceRecognitionAddExampleResponse(TypedDict):
@@ -29,3 +35,38 @@ class FaceRecognitionListExamplesResponse(TypedDict):
     page_size: int
     total_pages: int
     total_elements: int
+
+
+class FaceRecognitionResponse(TypedDict):
+    """The default response from the FaceRecognition
+
+    Attributes:
+        similarity (float): How similar it is to the subject
+        subject (str): Subject name
+    """
+
+    similarity: float
+    subject: str
+
+
+class FaceDetectionResult(TypedDict):
+    """Type for the complete Face Detection response from the CompreFace REST API
+
+    Attributes:
+        age (FaceDetectionAgeResult): The result from the age plugin if used
+        gender (FaceDetectionGenderResult): The result from the gender plugin if used
+        mask (FaceDetectionMaskResult): The result from the mask plugin if used
+        embedding (List[float]): The result from the calculator plugin if used
+        box (FaceDetectionBoxResult): The bounding box of the detected face
+        landmarks (List[List[float]): The result from the landmarks plugin if used
+        execution_time (FaceDetectionExecutionTime): The execution time of all plugins
+    """
+
+    age: FaceDetectionAgeResult
+    gender: FaceDetectionGenderResult
+    mask: FaceDetectionMaskResult
+    embedding: List[float]
+    box: FaceDetectionBoxResult
+    landmarks: List[List[float]]
+    subjects: List[FaceRecognitionResponse]
+    execution_time: FaceDetectionExecutionTime
